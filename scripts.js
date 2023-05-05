035d17b5b3f8be18d1ca8f938a8c5e087b686a62
raquelferreira1 <raquel.riosferreira@gmail.com>
2023-05-05 19:11:29 -0300

    feat: acrescimo euro

--- scripts.js
+++ scripts.js
@@ -2,6 +2,7 @@ const button = document.getElementById('button-conversor')
 const select = document.getElementById('currency-select')
 
 const dolar = 5.2
+const euro = 5.9
 
 const convertValues = () => {
     const inputReais = document.getElementById('input-real').value
@@ -12,24 +13,33 @@ const convertValues = () => {
         { style: 'currency', currency: 'BRL' }
     ).format(inputReais);
 
-    currencyValueText.innerHTML = new Intl.NumberFormat('en-US',
-        { style: 'currency', currency: 'USD' }
-    ).format(inputReais / dolar);
+    if (select.value === 'US$ Dólar Americano') {
+        currencyValueText.innerHTML = new Intl.NumberFormat('en-US',
+            { style: 'currency', currency: 'USD' }
+        ).format(inputReais / dolar)
+    }
+
+    if (select.value === '€ Euro') {
+        currencyValueText.innerHTML = new Intl.NumberFormat('de-DE',
+            { style: 'currency', currency: 'EUR' }
+        ).format(inputReais / euro)
+    }
 }
 
 const changeCurrency = () => {
     const currencyName = document.getElementById('currency-name')
     const currencyImg = document.getElementById('currency-img')
-    
-    if (select.value === 'US$ Dólar Americano'){
+
+    if (select.value === 'US$ Dólar Americano') {
         currencyName.innerHTML = 'Dólar'
         currencyImg.src = './assets/estados-unidos.png'
     }
 
-    if (select.value === '€ Euro'){
+    if (select.value === '€ Euro') {
         currencyName.innerHTML = 'Euro'
         currencyImg.src = './assets/euro.png'
     }
+    convertValues()
 }
 
 button.addEventListener('click', convertValues)
