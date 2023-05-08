830c4e3b74e4997eefe562e5bbf3fa5609b25b0d
raquelferreira1 <raquel.riosferreira@gmail.com>
2023-05-08 19:29:35 -0300

    feat: add api

--- scripts.js
+++ scripts.js
@@ -1,13 +1,16 @@
 const button = document.getElementById('button-conversor')
 const select = document.getElementById('currency-select')
 
-const dolar = 5.2
-const euro = 5.9
-
-const convertValues = () => {
+const convertValues = async () => {
     const inputReais = document.getElementById('input-real').value
     const realValueText = document.getElementById('real-value-text')
     const currencyValueText = document.getElementById('currency-value-text')
+
+    const api = await fetch("https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL").then(response => response.json())
+
+    const dolar = api.USDBRL.high
+    const euro = api.EURBRL.high
+    const bitcoin = api.BTCBRL.high
 
     realValueText.innerHTML = currencyValueText.innerHTML = new Intl.NumberFormat('pt-BR',
         { style: 'currency', currency: 'BRL' }
@@ -24,6 +27,12 @@ const convertValues = () => {
             { style: 'currency', currency: 'EUR' }
         ).format(inputReais / euro)
     }
+
+    if (select.value === 'Bitcoin') {
+        currencyValueText.innerHTML = new Intl.NumberFormat('en-US',
+            { style: 'currency', currency: 'BTC' }
+        ).format(inputReais / bitcoin * 1000)
+    }
 }
 
 const changeCurrency = () => {
@@ -39,6 +48,12 @@ const changeCurrency = () => {
         currencyName.innerHTML = 'Euro'
         currencyImg.src = './assets/euro.png'
     }
+
+    if (select.value === 'Bitcoin') {
+        currencyName.innerHTML = 'Bitcoin'
+        currencyImg.src = './assets/bitcoin.png'
+    }
+
     convertValues()
 }
 
